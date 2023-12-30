27930112006199752f36ffa5f9046d8ffa54ce94
Danila Krasov <67839759+Fr0stFree@users.noreply.github.com>
2023-12-30 14:51:28 +0300

    add redis broker (#7)
    * add redis broker
    
    * switch default broker to rabbitmq

--- event_collector/app/src/producers/__init__.py
+++ event_collector/app/src/producers/__init__.py
@@ -1,3 +1,4 @@
 from .base import AbstractBroker
 from .kafka_broker import KafkaBroker
 from .rabbit_broker import RabbitBroker
+from .redis_broker import RedisBroker
---
+++ event_processor/app/src/consumers/redis_broker.py
@@ -0,0 +1,22 @@
+from typing import Generator, Any
+
+import redis
+
+from .base import AbstractBroker
+
+
+class RedisBroker(AbstractBroker):
+    def __init__(self, host: str, port: int, channel_name: str) -> None:
+        self._channel_name = channel_name
+        self._connection = redis.Redis(host=host, port=port)
+        print(f"Connected to redis. Ready to receive messages from channel '{self._channel_name}'")
+
+    def receive(self) -> Generator[Any, None, None]:
+        pubsub = self._connection.pubsub()
+        pubsub.subscribe(self._channel_name)
+
+        for message in pubsub.listen():
+            if message['type'] == 'message':
+                message = message['data'].decode('utf-8')
+                print(f"Received message: {message}")
+                yield message
